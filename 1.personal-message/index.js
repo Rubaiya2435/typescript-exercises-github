86318ed344b8749d8542a691ca745dd737ea9b28
Rubaiya2435 <164077424+Rubaiya2435@users.noreply.github.com>
2024-03-22 13:14:10 -0300

    exercises of typescript

---
+++ 1.personal-message/index.js
@@ -0,0 +1,4 @@
+let personName = "Affan";
+let message = "Hello! would you like to play some online games?";
+console.log(personName + " " + message);
+export {};
---
+++ 1.personal-message/index.ts
@@ -0,0 +1,5 @@
+import inquirer from "inquirer";
+let personName: string = "Affan"; 
+let message: string = "Hello! would you like to play some online games?"
+console.log(personName + " " + message);
+
